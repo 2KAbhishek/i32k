61a07017e65018b965b73cb5f312a23b8f8ebe17
Abhishek Keshri <25423853+2KAbhishek@users.noreply.github.com>
2026-08-22 06:26:13 +0530

    perf: remove xfce4-power-manager to save background memory

--- setup.sh
+++ setup.sh
@@ -5,13 +5,11 @@ set -e
 # Detect if the system is Arch-based
 if [ -f /etc/arch-release ] || command -v pacman &>/dev/null; then
     echo "Arch Linux detected. Preparing to install dependencies..."
-
     sudo pacman -S --needed \
         autotiling i3-wm i3status i3lock xss-lock rofi rofi-emoji kitty xclip cliphist \
         swappy maim brightnessctl xorg-xsetroot lxqt-policykit networkmanager jq playerctl \
-        xorg-xauth xorg-xinit xorg-xset xorg-xinput picom \
-        xfce4-power-manager thunar tumbler thunar-archive-plugin thunar-volman \
-        ttf-firacode-nerd ttf-roboto python
+        xorg-xauth xorg-xinit xorg-xset xorg-xinput picom ttf-firacode-nerd ttf-roboto python \
+        thunar tumbler thunar-archive-plugin thunar-volman
 else
     echo "Warning: This script only supports package installation on Arch-based systems."
 fi
